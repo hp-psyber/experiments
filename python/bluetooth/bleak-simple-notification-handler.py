fd657d28f6584f521ffa1ffb15948ee54dc3bcf4
Haripriya <haripriya@psyberhealth.com>
2022-09-15 22:19:46 +0530

    notification handler

---
+++ python/bluetooth/bleak-simple-notification-handler.py
@@ -0,0 +1,43 @@
+"""
+Notifications
+-------------
+How to add notifications to a characteristic and handle the responses.
+"""
+
+import sys
+import asyncio
+import platform
+
+from bleak import BleakClient
+
+
+# you can change these to match your device or override them from the command line
+CHARACTERISTIC_UUID = "f000aa65-0451-4000-b000-000000000000"
+ADDRESS = (
+    "24:71:89:cc:09:05"
+    if platform.system() != "Darwin"
+    else "B9EA5233-37EF-4DD6-87A8-2A875E821C46"
+)
+
+
+def notification_handler(sender, data):
+    """Prints the data received."""
+    print("{0}: {1}".format(sender, data))
+
+
+async def main(address, char_uuid):
+    async with BleakClient(address) as client:
+        print(f"Connected: {client.is_connected}")
+
+        await client.start_notify(char_uuid, notification_handler)
+        await asyncio.sleep(5.0)
+        await client.stop_notify(char_uuid)
+
+
+if __name__ == "__main__":
+    asyncio.run(
+        main(
+            sys.argv[1] if len(sys.argv) > 1 else ADDRESS,
+            sys.argv[2] if len(sys.argv) > 2 else CHARACTERISTIC_UUID,
+        )
+    )
