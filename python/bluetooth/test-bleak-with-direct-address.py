b1ef563e0b0136b307026505e9a8dc2f408621e5
Haripriya <haripriya@psyberhealth.com>
2022-09-28 18:52:53 +0530

    removed unwanted code

--- python/bluetooth/test-bleak-with-direct-address.py
+++ python/bluetooth/test-bleak-with-direct-address.py
@@ -12,12 +12,6 @@ from bleak.exc import BleakError
 uuid_battery_service = '0000180f-0000-1000-8000-00805f9b34fb'
 uuid_battery_level_characteristic = '00002a19-0000-1000-8000-00805f9b34fb'
 
-ADDRESS = (
-    "56:21:1C:BE:DD:44"
-    if platform.system() != "Darwin"
-    else "B9EA5233-37EF-4DD6-87A8-2A875E821C46"
-)
-
 async def main(wanted_name: str):
     device = await BleakScanner.find_device_by_filter(
         lambda d, ad: d.name and d.name.lower() == wanted_name.lower()
