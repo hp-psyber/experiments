845cc1edb0b74d5f4b61af4795087917552f73f5
Haripriya <haripriya@psyberhealth.com>
2022-09-28 11:38:08 +0530

    updated bleakClient code

--- python/bluetooth/test-bleak-with-direct-address.py
+++ python/bluetooth/test-bleak-with-direct-address.py
@@ -20,21 +20,7 @@ ADDRESS = (
 
 
 async def main(ble_address: str):
-    device = await BleakScanner.find_device_by_address(ble_address, timeout=20.0)
-    if not device:
-        raise BleakError(f"A device with address {ble_address} could not be found.")
-    
-    async with BleakClient(device) as client:
-        print("Services:")
-        for service in client.services:
-            print(service)
-    address = ble_address
-
-    async with BleakClient(address) as client:
-        svcs = await client.get_services()
-        print("Services:")
-        for service in svcs:
-            print(service)
+    address = ADDRESS
 
     async with BleakClient(address) as client:
         svcs = await client.get_services()
